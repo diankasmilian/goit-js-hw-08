f9215d88c4d166695c6ff867000e3df1e57224bb
Diana Smilian <dianka.kulakevich@gmail.com>
2023-04-27 18:55:37 +0300

    finish task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const cardGallery = createImageCard(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardGallery);
+const lightbox = new SimpleLightbox('.gallery__link', { captionDelay: 250,
+   scrollZoom: false,
+   captionsData: 'alt' });
+
+function createImageCard(items) {
+   return items.map(({preview, original, description}) => {
+      return `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" 
+         src="${preview}" 
+         alt="${description}" 
+          />
+      </a>
+   </li>
+      `
+   }).join('');
+   };
