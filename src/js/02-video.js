552ab4d658f7ce6d569246af2eb9e635ea2e4344
Diana Smilian <dianka.kulakevich@gmail.com>
2023-04-27 19:41:35 +0300

    finish task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+player.on('timeupdate', throttle(function(data) {
+   localStorage.setItem('videoplayer-current-time', data.seconds);
+}), 1000);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
