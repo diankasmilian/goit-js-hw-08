02981e1a86d7cb63443d47d5d22176488ca48183
diankasmilian <119013441+diankasmilian@users.noreply.github.com>
2023-04-29 18:50:06 +0000

    Deploying to gh-pages from @ diankasmilian/goit-js-hw-08@0fb165a30a14b9bb08325fa04e4343e66e684d7e 🚀

--- 03-feedback.4337e32f.js
+++
@@ -1,2 +0,0 @@
-({input:document.querySelector("input"),button:document.querySelector("button")}).input.addEventListener("input",(function(){}));
-//# sourceMappingURL=03-feedback.4337e32f.js.map
--- 03-feedback.da3b0480.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("input"),document.querySelector("button")][0].addEventListener("input",(function(){}));
-//# sourceMappingURL=03-feedback.da3b0480.js.map
