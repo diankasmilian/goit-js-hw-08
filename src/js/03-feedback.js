0fb165a30a14b9bb08325fa04e4343e66e684d7e
Diana Smilian <dianka.kulakevich@gmail.com>
2023-04-29 21:49:37 +0300

    finish tash 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,40 @@
+import throttle from 'lodash.throttle';
 
 const refs = {
+form: document.querySelector('.feedback-form'),
 input: document.querySelector('input'),
+textarea: document.querySelector('textarea'),
 button: document.querySelector('button'),
 }
 
-refs.input.addEventListener('input', onInputText)
+refs.input.addEventListener('input', throttle(onInputText, 500));
+refs.textarea.addEventListener('input', throttle(onInputText, 500));
+refs.form.addEventListener('submit', onButtonSubmit);
+window.addEventListener('load', onLoadPage);
 
-function onInputText () {
+function onInputText (e) {
+e.preventDefault();
 
+   localStorage.setItem("feedback-form-state", JSON.stringify({email: refs.input.value, message: refs.textarea.value}));
+
+};
+
+const date = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+function onLoadPage () {
+if (date) {
+   refs.input.value = date.email;
+   refs.textarea.value = date.message;
+};
+};
+
+function onButtonSubmit (e) {
+e.preventDefault();
+
+console.log(date);
+
+localStorage.removeItem("feedback-form-state");
+
+refs.input.value = "";
+refs.textarea.value = "";
 }
